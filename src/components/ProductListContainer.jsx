90398148e1c6bba33c6eaac8255d79a10f3da48d
Fabián Tureo <ftureo@lanacion.com.ar>
2024-07-19 00:12:22 -0300

    feat: ✨ invoke getProducts method, manipulate promise from this method, invoke loadProducts into useEffect and implement useState hook to control products in component

---
+++ src/components/ProductListContainer.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+import ProductList from "./ProductList";
+import { getProducts } from "../services/productService";
+
+const ProductListContainer = () => {
+    const [products, setProducts] = useState([]);
+    // const [isLoading, setIsLoading] = useState(true);
+
+    /**
+     * @function loadProducts
+     * @description Carga los productos de la API de FakeStoreAPI invocando a getProducts, resolviendo la promesa y actualiza el estado de products a través de setProducts
+     * @returns {Array} Retorna un array con los productos
+     */
+    const loadProducts = async () => {
+        const productsData = await getProducts();
+
+        setProducts(productsData);
+        // setIsLoading(false);
+    };
+
+    useEffect(() => {
+        loadProducts();
+    }, []);
+    return (
+        <>
+            <h1>Acá renderizamos el acumulado de productos</h1>
+            {products.length === 0 && <h1>Todavía no tenemos los productos</h1>}
+            <ProductList products={products} />
+        </>
+    );
+};
+
+export default ProductListContainer;
