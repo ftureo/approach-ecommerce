5a397728ab022def88112fb0adba54692b53f0c1
Fabián Tureo <ftureo@lanacion.com.ar>
2024-07-19 00:06:03 -0300

    feat: ✨ implement transversal constants

---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+const contants = {
+    BASE_URL_FAKESTOREAPI: 'https://fakestoreapi.com',
+    INTERNAL_SERVER_ERROR: 'No estamos pudiendo contactarnos con FakeStoreAPI. Por favor, intentá más tarde o contactá al equipo de desarrollo.',
+    ENDPOINT_PRODUCTS: "/products"
+}
+export default contants;
