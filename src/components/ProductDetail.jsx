8a4461ac01542f153a9fd8a51e37e875d7bce79b
Fabián Tureo <ftureo@lanacion.com.ar>
2024-07-25 19:29:06 -0300

    feat: ✨ add detail component

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,7 +1,15 @@
-const ProductDetail = () => {
+/* eslint-disable react/prop-types */
+const ProductDetail = ({ product }) => {
     return (
         <>
             <h4>Product Detail</h4>
+            <div>
+                <img src={product.image} alt={product.title} />
+                <h2>{product.title}</h2>
+                <p>{product.description}</p>
+                <p>{product.category}</p>
+                <p>${product.price}</p>
+            </div>
         </>
     );
 };
