ef8b83d63f710ee8ff2edf0d034730addd82a21c
Fabián Tureo <ftureo@lanacion.com.ar>
2024-07-25 17:31:19 -0300

    feat: ✨ create new route available to use dynamic category param

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import ProductListContainer from "./components/ProductListContainer";
 import ProductDetailContainer from "./components/ProductDetailContainer";
+import Navbar from "./components/Navbar";
 
 function App() {
     return (
         <>
             <h1>E-commerce</h1>
+            <Navbar />
             <Routes>
                 <Route path="/products" element={<ProductListContainer />} />
                 <Route
-                    path="/product/detail"
+                    path="/products/category/:categoryId"
+                    element={<ProductListContainer />}
+                />
+                <Route
+                    path="/product/detail/:productId"
                     element={<ProductDetailContainer />}
                 />
             </Routes>
