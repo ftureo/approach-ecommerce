c6e7f62bacc20348c8711c8b3d15669cd1bf7d2d
Fabián Tureo <ftureo@lanacion.com.ar>
2024-07-25 19:31:03 -0300

    feat: ✨ implement useParams and develop business logic for request one product

--- src/components/ProductDetailContainer.jsx
+++ src/components/ProductDetailContainer.jsx
@@ -1,10 +1,29 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import ProductDetail from "./ProductDetail";
+import { getProductById } from "../services/productService";
 
 const ProductDetailContainer = () => {
+    const [productDetail, setProductDetail] = useState(null);
+    const { productId } = useParams();
+    console.log({ productId });
+
+    useEffect(() => {
+        const loadProductDetail = async () => {
+            const productDetailData = await getProductById(productId);
+            setProductDetail(productDetailData);
+        };
+        loadProductDetail();
+    }, [productId]);
+
     return (
         <div>
             <h3>Product Detail Container</h3>
-            <ProductDetail />
+            {!productDetail ? (
+                <h2>Cargando detalle del producto...</h2>
+            ) : (
+                <ProductDetail product={productDetail} />
+            )}
         </div>
     );
 };
