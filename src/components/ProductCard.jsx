e477c596557f765da918046cd350b8aeff1191d6
Fabián Tureo <ftureo@lanacion.com.ar>
2024-07-25 19:29:52 -0300

    feat: ✨ add redirect using Link component from RRD

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,20 +1,27 @@
 /* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
+
 import "../styles/ProductCard.css";
 
 const ProductCard = ({ product }) => {
     console.log({ product });
     return (
         <div className="product-card">
-            <img
-                src={product.image}
-                alt={product.title}
-                className="product-image"
-            />
-            <div className="product-details">
-                <h2 className="product-title">{product.title}</h2>
-                <p className="product-price">${product.price}</p>
-                <p className="product-description">{product.description}</p>
-            </div>
+            <Link to={`/product/detail/${product.id}`}>
+                <img
+                    src={product.image}
+                    alt={product.title}
+                    className="product-image"
+                />
+                <div className="product-details">
+                    <h2 className="product-title">{product.title}</h2>
+                    <p className="product-price">${product.price}</p>
+                    <p className="product-description">{product.description}</p>
+                    <p className="product-description">
+                        <b>{product.category}</b>
+                    </p>
+                </div>
+            </Link>
         </div>
     );
 };
